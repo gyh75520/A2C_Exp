b61a10e9b41f4c6c18674872b162c8bf99f152ad
gyh75520 <734468130@qq.com>
2019-03-22 22:56:48 +0800

    fix env bug

--- env/gym-box-world/gym_boxworld/envs/Box_world_env.py
+++ env/gym-box-world/gym_boxworld/envs/Box_world_env.py
@@ -58,7 +58,7 @@ class BoxWoldEnv(gym.Env):
         # self.action_pos_dict = {0: [0, 0], 1: [-1, 0], 2: [1, 0], 3: [0, -1], 4: [0, 1]}
         # ignore noop action
         self._action_set = [2, 3, 4, 5]
-        self.action_space = spaces.Discrete(len(self._action_set) - 1)
+        self.action_space = spaces.Discrete(len(self._action_set))
         self.action_pos_dict = {0: [0, 0], 1: [-1, 0], 2: [1, 0], 3: [0, -1], 4: [0, 1]}
 
         # set observation space
